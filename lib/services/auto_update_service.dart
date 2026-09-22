4e76be196d205f0d797678f046adfae92cae7982
heshanga <heshanpvt@gmail.com>
2026-09-22 19:16:32 +0530

    Upload lib/services/auto_update_service.dart

--- lib/services/auto_update_service.dart
+++ lib/services/auto_update_service.dart
@@ -5,7 +5,7 @@ import 'package:url_launcher/url_launcher.dart';
 
 class AutoUpdateService {
   static const String updateJsonUrl = "https://rdmns.hesn.xyz/update.json";
-  static const int currentVersionCode = 14; // v1.1.4
+  static const int currentVersionCode = 15; // v1.1.5
 
   static Future<void> checkForUpdates(BuildContext context) async {
     try {
