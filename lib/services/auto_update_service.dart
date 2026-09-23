a56eb984e788e952a7c95c60f3ccd8012ef1f955
heshanga <heshanpvt@gmail.com>
2026-09-23 19:03:01 +0530

    Upload lib/services/auto_update_service.dart

--- lib/services/auto_update_service.dart
+++ lib/services/auto_update_service.dart
@@ -25,7 +25,7 @@ class AutoUpdateService {
 
         // Dynamically fetch installed app version code
         final packageInfo = await PackageInfo.fromPlatform();
-        final currentBuildNumber = int.tryParse(packageInfo.buildNumber) ?? 19;
+        final currentBuildNumber = int.tryParse(packageInfo.buildNumber) ?? 20;
 
         // Check if user previously dismissed this version
         final prefs = await SharedPreferences.getInstance();
